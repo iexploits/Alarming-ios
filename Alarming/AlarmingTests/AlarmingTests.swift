0f010f433323f77fb728e91fbeaa556be0bab972
허재 <gjwp1227@gmail.com>
2019-06-18 18:23:19 +0900

    init

---
+++ Alarming/AlarmingTests/AlarmingTests.swift
@@ -0,0 +1,34 @@
+//
+//  AlarmingTests.swift
+//  AlarmingTests
+//
+//  Created by Jae Heo on 18/06/2019.
+//  Copyright © 2019 iExploits. All rights reserved.
+//
+
+import XCTest
+@testable import Alarming
+
+class AlarmingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
