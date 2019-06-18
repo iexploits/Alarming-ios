e8299b29b3f037ef7f5e76c9ffa2856b3d532995
허재 <gjwp1227@gmail.com>
2019-06-18 23:27:50 +0900

    UI design for SnsFeedView

---
+++ Alarming/Alarming/UI/Views/SNSFeedView.swift
@@ -0,0 +1,29 @@
+//
+//  SNSFeedView.swift
+//  Alarming
+//
+//  Created by Jae Heo on 18/06/2019.
+//  Copyright © 2019 iExploits. All rights reserved.
+//
+
+import UIKit
+
+class SNSFeedView: UIView {
+    private let xibName = "SNSFeedView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    
+    private func commonInit(){
+        let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+}
