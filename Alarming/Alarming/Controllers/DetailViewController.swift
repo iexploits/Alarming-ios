8085f418979d08f26c42aacced4e27300ec13063
허재 <gjwp1227@gmail.com>
2019-06-19 23:30:40 +0900

    save

---
+++ Alarming/Alarming/Controllers/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Alarming
+//
+//  Created by Jae Heo on 19/06/2019.
+//  Copyright © 2019 iExploits. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class DetailViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+}
